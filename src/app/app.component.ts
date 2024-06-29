2462b906be26fd1d7f218866c625532411c54620
nicobolton2 <115894556+nicobolton2@users.noreply.github.com>
2024-06-29 01:17:30 -0400

    boostrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PadreComponent } from './modules/familia/padre/padre.component';
 import { HermanoComponent } from './modules/familia/hermano/hermano.component';
+import { NavbarComponent} from './modules/website/navbar/navbar.component';
+import { FooterComponent} from './modules/website/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { HermanoComponent } from './modules/familia/hermano/hermano.component';
   imports: [
     RouterOutlet,
     PadreComponent,
-    HermanoComponent
+    HermanoComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
